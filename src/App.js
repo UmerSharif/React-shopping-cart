156f057381800ba713aa27be5ff791d68a392aec
umer <umer4167@hotmail.com>
2019-04-05 16:22:34 +0300

    pickup saving and tooltip added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import './App.css';
 import {Container, Row, Col} from 'react-bootstrap'
 import Button from 'react-bootstrap/Button';
 
+import Subtotal from './components/Subtotal'
+import PickupSavings from './components/PickupSavings'
+
+
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      total : 0,
+      totalSaving : -3.5
+    }
+  }
   render() {
     return (
       <div className="App">
         <Container className="purchase-card">
-        <h1>Hello</h1>
-        <Row>
-       <Col>1 of 2</Col>
-       <Col>2 of 2</Col>
-      </Row>
+        <h3 style={{marginBottom : '1.5em', textAlign: 'center'}}>Shopping Cart</h3>
+        <Subtotal price = {this.state.total.toFixed(2)} />
+        <PickupSavings price = {this.state.totalSaving} />
         </Container>
         </div>
     );

07fbf257c1d3e88822e23920650e05c1d74c5d80
umer <umer4167@hotmail.com>
2019-04-05 15:39:16 +0300

    card created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Container, Row, Col} from 'react-bootstrap'
 import Button from 'react-bootstrap/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Container className="purchase-card">
         <h1>Hello</h1>
-        <Button variant="secondary">Submit</Button>
-      </div>
+        <Row>
+       <Col>1 of 2</Col>
+       <Col>2 of 2</Col>
+      </Row>
+        </Container>
+        </div>
     );
   }
 }
